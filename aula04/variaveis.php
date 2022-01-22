3b4f6d6485de1778e815d55a79a9c29faeac0bad
KauanLeivingsonRomero <96146200+KauanLeivingsonRomero@users.noreply.github.com>
2022-01-22 15:23:35 -0300

    aula 04 - variaveis

---
+++ aula04/variaveis.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/projects/aulaPHP/_css/estilo.css">
+  <title>Variaveis em PHP</title>
+</head>
+<body>
+  <div>
+    <?php
+      $idade = 16;
+      // como forcar o tipo da variavel:
+      // $numero = (int) 04;
+      $nome = "Kauan";
+      echo "$nome tem $idade anos";
+    ?>
+  </div>
+</body>
+</html>
