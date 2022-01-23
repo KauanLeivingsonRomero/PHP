6aaac16fc315b49931017804242290a4d230bcc6
KauanLeivingsonRomero <96146200+KauanLeivingsonRomero@users.noreply.github.com>
2022-01-23 18:43:53 -0300

    valores personalizados

--- aula05/operadores.php
+++ aula05/operadores.php
@@ -10,9 +10,16 @@
 <body>
   <div>
     <?php 
-      $n1 = 3;
-      $n2 = 2;
+      $n1 = $_GET["a"];
+      $n2 = $_GET["b"];
+      echo "<h2>Valores recebidos: $n1 e $n2</h2>";
+      $m = ($n1 + $n2) / 2;
       echo "A soma vale ". ($n1 + $n2);
+      echo "<br/>A subtracao vale ". ($n1-$n2);
+      echo "<br/>A multiplicacao vale ". ($n1*$n2);
+      echo "<br/>A divisao vale ". ($n1/$n2);
+      echo "<br/>O modulo vale ". ($n1%$n2);
+      echo "<br/>A media vale ". $m;
     ?>
   </div>
 </body>
