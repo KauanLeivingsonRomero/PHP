0979624043be630e314f406f688fe40a98354c9e
KauanLeivingsonRomero <96146200+KauanLeivingsonRomero@users.noreply.github.com>
2022-01-23 17:13:49 -0300

    concatenando string com aritmetica

---
+++ aula05/operadores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>operacoes aritmeticas</title>
+  <link rel="stylesheet" href="/projects/aulaPHP/_css/estilo.css">
+</head>
+<body>
+  <div>
+    <?php 
+      $n1 = 3;
+      $n2 = 2;
+      echo "A soma vale ". ($n1 + $n2);
+    ?>
+  </div>
+</body>
+</html>
